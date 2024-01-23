d2fef570f574fb59a8d9132d216d47f6e6fea4d0
luckydipper <qhrqufdlek@naver.com>
2024-01-23 18:08:57 +0900

    [FIX] - current file path

--- execute.bash
+++ execute.bash
@@ -5,6 +5,6 @@ docker run -it --rm \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="/home/airlab/Desktop/workspace/GTSAM_tutorial:/root/GTSAM_tutorial" \
+    --volume="./GTSAM_tutorial:/root/GTSAM_tutorial" \
     gtsam_tutorial:0.6
 xhost -local:root
